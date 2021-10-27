08c219a8d842ee7632f2020d8e469da960373461
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-10-27 13:26:57 +1100

    - Built in OS modules - Time: 1hr 04 mins - https://www.youtube.com/watch?v=Oe421EPjeBE

--- 01-Practice/04-names.js
+++
@@ -1,7 +0,0 @@
-// local
-const secret = 'SUPER SECRET'
-// share
-const john = 'john'
-const peter = 'peter'
-
-module.exports = { john, peter } // We share onlt these two while keeping secret local
