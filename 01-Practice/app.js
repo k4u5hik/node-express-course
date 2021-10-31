4ad9ff76cc44f6f320e7fedda2526f785c3c5b28
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-01 09:21:45 +1100

    - Template Literals did not work on Safari - Code was correct. - HTML did work on the Chrome Browser
    - Time: 1hr 45 mins
    - https://www.youtube.com/watch?v=Oe421EPjeBE

--- 01-Practice/app.js
+++ 01-Practice/app.js
@@ -1,9 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log(req.url);
-    res.write('Welcome to our homepage');
-    res.end()
+    if(req.url === '/') {
+    res.end('Hello World');
+    } else if (req.url === '/about'){
+    res.end(`Here is our short history`)
+    } else {
+    res.end(`
+    <h1>OOPS!</h1>
+    <p>We can't seem to find the page you are looking for.</p>
+    <a href="/"> Back to Home</a>
+    `)
+    }
 })
 
 server.listen(3000)
