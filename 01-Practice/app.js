05ce62e82c7da04909f1290e888a9b0f0b134ee6
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-03 10:51:44 +1100

    - Try and catch block - When directory path is incorrect, nodemon gives an error
    - Time: 3hr 04 mins
    - https://www.youtube.com/watch?v=Oe421EPjeBE

--- 01-Practice/app.js
+++ 01-Practice/app.js
@@ -1,11 +1,36 @@
 const {readFile} = require('fs');
+const path = require("path");
 
-readFile('./content/first.txt','utf8',(err,data)=>{
-    if (err){
-        return
-    } else {
-        console.log(data);
-    }
+const getText = (path) => {
+    return new Promise((resolve, reject) => {
+        readFile(path, 'utf8', (err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(data);
+            }
+        })
     })
+}
 
-// Ran the first.txt file data on the terminal console log successfully.
+// getText('./content/first.txt')// If I change the path to '../content/second.txt' it will return an error
+//     .then((result) => console.log(result))
+//     .catch((err) => console.log(err))
+
+const start = async() =>{
+    try {                                           // Try and catch block
+        const first = await getText('./content/first.txt');
+        console.log(first);
+    } catch (error) {
+        console.log(error);
+    }}
+
+
+start()
+
+// Set up a promise chain
+// Setup an async await function
+// Call the async await function
+// Log the result
+// Log the error
+
