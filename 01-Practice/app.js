90f512703f613ada4780574baa4b5639f08153cc
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-03 11:04:06 +1100

    - Write file one and two to third file which was generated.
    - Time: 3hr 10 mins
    - https://www.youtube.com/watch?v=Oe421EPjeBE

--- 01-Practice/app.js
+++ 01-Practice/app.js
@@ -1,33 +1,38 @@
-const {readFile} = require('fs');
-const path = require("path");
-
-const getText = (path) => {
-    return new Promise((resolve, reject) => {
-        readFile(path, 'utf8', (err, data) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(data);
-            }
-        })
-    })
-}
-
-// getText('./content/first.txt')// If I change the path to '../content/second.txt' it will return an error
-//     .then((result) => console.log(result))
-//     .catch((err) => console.log(err))
+const {readFile,writeFile} = require('fs');
+const util = require("util");
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 
 const start = async() =>{
     try {                                           // Try and catch block
-        const first = await getText('./content/first.txt');
-        const second = await getText('./content/second.txt'); // Added second file
+        const first = await readFilePromise('./content/first.txt','utf8');
+        const second = await readFilePromise('./content/second.txt','utf8'); // Added second file
+        await writeFilePromise('./content/result-mind-grenade.txt',`This is awesome!: ${first} ${second}`); // Generated result file and added the two files
         console.log(first,second); //Logs out the two files
     } catch (error) {
         console.log(error);
     }}
 
-
 start()
+
+
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf8', (err, data) => {
+//             if (err) {
+//                 reject(err)
+//             } else {
+//                 resolve(data);
+//             }
+//         })
+//     })
+// }
+
+
+// getText('./content/first.txt')// If I change the path to '../content/second.txt' it will return an error
+//     .then((result) => console.log(result))
+//     .catch((err) => console.log(err))
+
 
 // Set up a promise chain
 // Setup an async await function
