93b38afd069a21616e3a2a4c541e976f884a4bc9
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-03 12:29:05 +1100

    - Added arguments to the event - console logged the response
    - Time: 3hr 18 mins
    - https://www.youtube.com/watch?v=Oe421EPjeBE

--- 01-Practice/app.js
+++ 01-Practice/app.js
@@ -4,8 +4,14 @@ const EventEmitter = require('events') // Import EventEmitter
 
 const customEmitter = new EventEmitter() // Create new EventEmitter
 
-customEmitter.on('response',()=>{ // Listen for response event) - on method
-    console.log('Response received')
+customEmitter.on('response',(name,id)=>{ // Listen for response event) - on method
+    console.log(`Response received ${name} with id ${id}`)
 })
 
-customEmitter.emit('response')// Emit response event - emit method
+customEmitter.on('response',()=>{ // You can add as many events as you want. Order of these events matters
+    console.log('Some other logic received')
+})
+
+customEmitter.emit('response','kaushik',38)// Emit response event - emit method
+
+// If you listen to response after the emit then it would not work.
