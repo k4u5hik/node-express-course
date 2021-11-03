14ea9f4e22eff5ead25f8a46e5d0c21097a23efe
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-04 10:24:20 +1100

    - filestream.pipe(writestream); - We can read and write to the same stream in chunks. - If you check on the console log, you will see that the chunks are being read in chunks of 90kb.
    - Time: 3hr 33 mins
    - https://www.youtube.com/watch?v=Oe421EPjeBE

--- 01-Practice/app.js
+++ 01-Practice/app.js
@@ -1,6 +1,6 @@
 const {createReadStream} = require('fs');
 
-const stream = createReadStream('../content/big.txt',{
+const stream = createReadStream('./content/big.txt',{
     highWaterMark: 90000},
     {encoding: 'utf8'}
 );
@@ -10,6 +10,8 @@ const stream = createReadStream('../content/big.txt',{
 // highWaterMark is a property of the stream. Controls how much data is read at a time.
 // const stream = createReadStream('./content/big.txt', {highWaterMark: 90000}); - Will be 90kb, Filesize is 169 kb
 // const stream = createReadStream('./content/big.txt', {encoding: 'utf8'});
+// filestream.pipe(writestream); - We can read and write to the same stream in chunks.
+// If you check on the console log, you will see that the chunks are being read in chunks of 90kb.
 
 stream.on('data',(result)=>{
     console.log(result);
