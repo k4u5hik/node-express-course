85850b2a07488a6a350dd7a35985e91d3dafd173
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-03 12:21:31 +1100

    NodeJS Events: - Event driven programming - Basic setup for events
    - Time: 3hr 18 mins
    - https://www.youtube.com/watch?v=Oe421EPjeBE

--- 01-Practice/app.js
+++ 01-Practice/app.js
@@ -1,42 +1,11 @@
-const {readFile,writeFile} = require('fs').promises
-// const util = require("util");
-// const readFilePromise = util.promisify(readFile);
-// const writeFilePromise = util.promisify(writeFile);
+//Basic setup when it comes to events
 
-const start = async() =>{
-    try {                                           // Try and catch block
-        const first = await readFile('./content/first.txt','utf8');
-        const second = await readFile('./content/second.txt','utf8'); // Added second file
-        await writeFile('./content/result-mind-grenade.txt',`This is awesome!: ${first} ${second}`,{flag:'a'} ); // Generated result file and added the two files
-        console.log(first,second); //Logs out the two files
-    } catch (error) {
-        console.log(error);
-    }}
+const EventEmitter = require('events') // Import EventEmitter
 
-start()
+const customEmitter = new EventEmitter() // Create new EventEmitter
 
+customEmitter.on('response',()=>{ // Listen for response event) - on method
+    console.log('Response received')
+})
 
-// const getText = (path) => {
-//     return new Promise((resolve, reject) => {
-//         readFile(path, 'utf8', (err, data) => {
-//             if (err) {
-//                 reject(err)
-//             } else {
-//                 resolve(data);
-//             }
-//         })
-//     })
-// }
-
-
-// getText('./content/first.txt')// If I change the path to '../content/second.txt' it will return an error
-//     .then((result) => console.log(result))
-//     .catch((err) => console.log(err))
-
-
-// Set up a promise chain
-// Setup an async await function
-// Call the async await function
-// Log the result
-// Log the error
-
+customEmitter.emit('response')// Emit response event - emit method
