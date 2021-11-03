cf9a4f315066fad8d338ae3de944a2acdb352120
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-04 10:08:07 +1100

    - Stream basics - Added some comments
    - Time: 3hr 33 mins
    - https://www.youtube.com/watch?v=Oe421EPjeBE

--- 01-Practice/app.js
+++ 01-Practice/app.js
@@ -1,13 +1,18 @@
 const {createReadStream} = require('fs');
 
-const stream = createReadStream('./content/big.txt');
+const stream = createReadStream('../content/big.txt',{
+    highWaterMark: 90000},
+    {encoding: 'utf8'}
+);
 
 // default 64kb buffer size
 // last buffer will be smaller as it is the end of the file/remainder.
 // highWaterMark is a property of the stream. Controls how much data is read at a time.
-// const stream = createReadStream('./content/big.txt', {highWaterMark: 90000});
+// const stream = createReadStream('./content/big.txt', {highWaterMark: 90000}); - Will be 90kb, Filesize is 169 kb
 // const stream = createReadStream('./content/big.txt', {encoding: 'utf8'});
 
 stream.on('data',(result)=>{
     console.log(result);
 })
+
+stream.on('error',(err)=> console.log(err));
