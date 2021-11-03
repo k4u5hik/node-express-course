136c87bfa51bc8710c1def05251d36cbe933c32e
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-03 12:35:53 +1100

    - http module - server.on event - server.listen event - works on localhost
    - Time: 3hr 18 mins
    - https://www.youtube.com/watch?v=Oe421EPjeBE

--- 01-Practice/app.js
+++ 01-Practice/app.js
@@ -1,17 +1,15 @@
-//Basic setup when it comes to events
+const http = require('http')
 
-const EventEmitter = require('events') // Import EventEmitter
+// const server = http.createServer((req, res) => {
+//   res.end('Welcome')
+// })
 
-const customEmitter = new EventEmitter() // Create new EventEmitter
-
-customEmitter.on('response',(name,id)=>{ // Listen for response event) - on method
-    console.log(`Response received ${name} with id ${id}`)
+// Using Event Emitter API
+const server = http.createServer()
+// emits request event
+// subcribe to it / listen for it / respond to it
+server.on('request', (req, res) => {
+    res.end('Welcome Kaushik')
 })
 
-customEmitter.on('response',()=>{ // You can add as many events as you want. Order of these events matters
-    console.log('Some other logic received')
-})
-
-customEmitter.emit('response','kaushik',38)// Emit response event - emit method
-
-// If you listen to response after the emit then it would not work.
+server.listen(3000)
