519c821e3ed0b8553cc3c4f721479857466d617c
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-05 16:22:57 +1100

    - Changed from plain text to html - http status codes https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
    4 hr 20 mins
    
    - https://www.youtube.com/watch?v=Oe421EPjeBE

--- 02-Practice/app.js
+++ 02-Practice/app.js
@@ -1,12 +1,12 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello World\n')
+  res.writeHead(200, { 'Content-Type': 'text/html' }) // We provide metadata about the response , 200 means that the request was successful
+  res.write('<h1> Hello World\n </h1>')                           // Changed from plain text to html
+    res.end()                                                           // We end the response
       console.log('User hit the server')
 })
 
 server.listen(3000, () => {
   console.log('Server running at http://localhost:3000/')
 })
-
