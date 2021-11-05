ab4d4480f06cfc1d2ce4e7617d2e6d45cd51bca6
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-05 17:06:24 +1100

    - A media type (also known as a Multipurpose Internet Mail Extensions or MIME type) indicates the nature and format of a document, file, or assortment of bytes. MIME types are defined and standardized in IETF's RFC 6838.
    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
    
    4 hr 23 mins
    
    - https://www.youtube.com/watch?v=Oe421EPjeBE

--- 02-Practice/app.js
+++ 02-Practice/app.js
@@ -1,9 +1,9 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' }) // We provide metadata about the response , 200 means that the request was successful
-  res.write('<h1> Hello World\n </h1>')                           // Changed from plain text to html
-    res.end()                                                           // We end the response
+  res.writeHead(200, { 'Content-Type': 'text/html' })   // We provide metadata about the response, 200 means that the request was successful
+  res.write('<h1> Hello World\n </h1>')                             // Changed from plain text to html
+    res.end()                                                             // We end the response
       console.log('User hit the server')
 })
 
