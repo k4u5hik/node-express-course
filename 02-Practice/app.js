c015a8ac73a65ea318a6ad7e5db8ef82ccb1ce09
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-06 20:20:41 +1100

    - setup if and else if condition - setup about page and 404 page - 404 page uses statusCode of 404, not 200
    4 hr 30 mins
    
    - https://www.youtube.com/watch?v=Oe421EPjeBE

--- 02-Practice/app.js
+++ 02-Practice/app.js
@@ -1,13 +1,30 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  console.log(req.method, req.url)
-  res.writeHead(200, { 'Content-Type': 'text/html' })   // We provide metadata about the response, 200 means that the request was successful
-  res.write('<h1> Hello World\n </h1>' +
-      'This is my website')                                               // Changed from plain text to html
+  //console.log(req.method, req.url)
+  const url = req.url;
+  //home page
+  if (url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/html'})   // We provide metadata about the response, 200 means that the request was successful
+    res.write('<h1> Hello World\n </h1>' +
+        'This is my website')                                               // Changed from plain text to html
     res.end()                                                             // We end the response
-      console.log('User hit the server')
-})
+    console.log('User hit the server')
+    //about page
+  } else if (url === '/about'){
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write('<h1> About Page\n </h1>' +
+        'This is the about page')
+    res.end()
+  } else {
+    //404 page
+    res.writeHead(404, {'Content-Type': 'text/html'})
+    res.write('<h1>404 Page not found</h1>')
+    res.end()
+  }
+
+
+  })
 
 server.listen(3000, () => {
   console.log('Server running at http://localhost:3000/')
