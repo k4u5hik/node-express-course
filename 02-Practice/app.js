acef7bc91e699bf93638a18bd10beef6ae558972
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-06 20:12:47 +1100

    - console.log(req.method, req.url) - Once the user hits the website, console log will show GET method. - url will be the resource you are trying to GET.
    4 hr 27 mins
    
    - https://www.youtube.com/watch?v=Oe421EPjeBE

--- 02-Practice/app.js
+++ 02-Practice/app.js
@@ -1,8 +1,10 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
+  console.log(req.method, req.url)
   res.writeHead(200, { 'Content-Type': 'text/html' })   // We provide metadata about the response, 200 means that the request was successful
-  res.write('<h1> Hello World\n </h1>')                             // Changed from plain text to html
+  res.write('<h1> Hello World\n </h1>' +
+      'This is my website')                                               // Changed from plain text to html
     res.end()                                                             // We end the response
       console.log('User hit the server')
 })
