a81b30b32d28b4302a9151125285fbc5340e3551
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-07 19:36:36 +1100

    - created new const for styles.css, logo.svg, browser-app.js - website now loading all the resources properly and looking good.
    4 hr 45 mins
    
    - https://www.youtube.com/watch?v=Oe421EPjeBE

--- 02-Practice/app.js
+++ 02-Practice/app.js
@@ -3,6 +3,9 @@ const {readFileSync} = require('fs')
 
 // get all the files in the directory
 const homePage = readFileSync('./navbar-app/index.html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeImage = readFileSync('./navbar-app/logo.svg')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
 
 const server = http.createServer((req, res) => {
   //console.log(req.method, req.url)
@@ -13,13 +16,34 @@ const server = http.createServer((req, res) => {
     res.write(homePage)                                               // Changed from plain text to html
     res.end()                                                             // We end the response
     console.log('User hit the server')
-    //about page
-  } else if (url === '/about'){
+  }
+  //about page
+  else if (url === '/about'){
     res.writeHead(200, {'Content-Type': 'text/html'})
     res.write('<h1> About Page\n </h1>' +
         'This is the about page')
     res.end()
-  } else {
+  }
+  //styles page
+  else if (url === '/styles.css'){
+    res.writeHead(200, {'Content-Type': 'text/css'})
+    res.write(homeStyles)
+    res.end()
+  }
+  //image page
+  else if (url === '/logo.svg'){
+    res.writeHead(200, {'Content-Type': 'image/svg+xml'})
+    res.write(homeImage)
+    res.end()
+  }
+  //logic page
+  else if (url === '/browser-app.js'){
+    res.writeHead(200, {'Content-Type': 'application/javascript'})
+    res.write(homeLogic)
+    res.end()
+  }
+  else
+  {
     //404 page
     res.writeHead(404, {'Content-Type': 'text/html'})
     res.write('<h1>404 Page not found</h1>')
