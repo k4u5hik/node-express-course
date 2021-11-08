158c0e3d552ae993777b63dc9f18c675ec3ec590
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-08 16:47:05 +1100

    - Basic express setup example - HTTP Methods GET - Read Data POST - Insert Data PUT - Update Data DELETE - Delete Data
    4 hr 51 mins
    
    - https://www.youtube.com/watch?v=Oe421EPjeBE

--- 02-Practice/app.js
+++ 02-Practice/app.js
@@ -1,56 +1,30 @@
-const http = require('http')
-const {readFileSync} = require('fs')
+const express = require('express');
+const app = express()
+const port = 3000
 
-// get all the files in the directory
-const homePage = readFileSync('./navbar-app/index.html')
-const homeStyles = readFileSync('./navbar-app/styles.css')
-const homeImage = readFileSync('./navbar-app/logo.svg')
-const homeLogic = readFileSync('./navbar-app/browser-app.js')
-
-const server = http.createServer((req, res) => {
-  //console.log(req.method, req.url)
-  const url = req.url;
-  //home page
-  if (url === '/') {
-    res.writeHead(200, {'Content-Type': 'text/html'})   // We provide metadata about the response, 200 means that the request was successful
-    res.write(homePage)                                               // Changed from plain text to html
-    res.end()                                                             // We end the response
-    console.log('User hit the server')
-  }
-  //about page
-  else if (url === '/about'){
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write('<h1> About Page\n </h1>' +
-        'This is the about page')
-    res.end()
-  }
-  //styles page
-  else if (url === '/styles.css'){
-    res.writeHead(200, {'Content-Type': 'text/css'})
-    res.write(homeStyles)
-    res.end()
-  }
-  //image page
-  else if (url === '/logo.svg'){
-    res.writeHead(200, {'Content-Type': 'image/svg+xml'})
-    res.write(homeImage)
-    res.end()
-  }
-  //logic page
-  else if (url === '/browser-app.js'){
-    res.writeHead(200, {'Content-Type': 'application/javascript'}) // can be text/javascript
-    res.write(homeLogic)
-    res.end()
-  }
-  else
-  {
-    //404 page
-    res.writeHead(404, {'Content-Type': 'text/html'})
-    res.write('<h1>404 Page not found</h1>')
-    res.end()
-  }
-  })
-
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/')
+app.get('/', (req, res) => {
+  console.log('user hit the resource')
+  res.status(200).send('Home Page')
 })
+
+app.get('/about',(req,res) => {
+  console.log('user hit the resource')
+  res.status(200).send('About Page')
+})
+
+app.all('*',(req,res) => {
+  console.log('user hit the resource')
+  res.status(404).send('<h1> 404 Page - Resource not found!</h1>')
+})
+
+app.listen(port, () => {
+  console.log('Server is running on port 3000');
+});
+
+//app.get
+//app.post
+//app.put
+//app.delete
+//app.all
+//app.use
+//app.listen
