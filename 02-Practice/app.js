8a8f97fdb6bd093047f435e246d236944da6ba5f
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-12 19:41:42 +1100

    Lesson 71 - You cannot send two responses in one/same request 0:47 ...linked to the callback function
    Lesson 72 - Middleware Setup
    - Middleware are functions that execute during the request to the server. Each middleware function has access to request and response object.
    - clearing out app.js
    - console.log(`${method} ${url} ${time}`); Terminal GET / 2021
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/25292564#overview

--- 02-Practice/app.js
+++ 02-Practice/app.js
@@ -1,62 +1,19 @@
 const express = require('express');
 const app = express();
-const {products} = require('./data');
 
+// req => middleware => res
 app.get('/', (req, res) => {
-  res.send('<h1> Home Page </h1> <a href="/api/products">products</a>');
+    const method = req.method;
+    const url = req.url;
+    const time = new Date().getFullYear();
+    console.log(`${method} ${url} ${time}`); // GET / 2021
+  res.send('Home');
 });
 
-app.get('/api/products', (req, res) => {
-    const newProducts = products.map((product)=>{
-        const {id,name,image} = product
-        return {id,name,image}
-    })
-  res.json(newProducts)
+app.get('/about', (req, res) => {
+    res.send('About');
 });
 
-app.get('/api/products/:productID', (req, res) => {
-//    console.log(req.params.productID)
-    const {productID} = req.params;
-    const singleProduct = products.find((product)=>{
-        return product.id === Number(productID)
-    })
-    if(!singleProduct) {
-        return res.status(404).send('Product does not exist!')
-    }
-    return res.json(singleProduct)
-});
-
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-       console.log(req.params)
-        res.send('Hello World!') // http://localhost:3000/api/products/1/reviews/1
-});
-
-app.get('/api/v1/query',(req,res)=>{
-    // console.log(req.query)
-    const {search, limit} = req.query
-    let sortedProducts = [...products]
-
-    if (search){
-        sortedProducts = sortedProducts.filter((product)=>{
-            return product.name.startsWith(search)
-        })
-    }
-    if (limit){
-        sortedProducts = sortedProducts.slice(0,limit)
-    }
-    if (sortedProducts.length < 1){
-        //return res.status(200).send('No products matched your search!')
-        return res.status(200).json({
-            success: true,
-            message: 'No products matched your search!',
-            data: []
-        }) //http://localhost:3000/api/v1/query?search=b&limit=1
-    }
-    res.status(200).json(sortedProducts)
-})
-
-// http://localhost:3000/api/v1/query?name=john&id=4 , Terminal { name: 'john', id: '4' }
-// http://localhost:3000/api/v1/query?search=a&limit=2, Terminal { search: 'a', limit: '2' }
 app.listen(3000, () => {
     console.log('Server is running on port 3000 http://localhost:3000');
 });
