1e66098779b29baa7095cfc647ffdb1cf6ea1bdb
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-12 19:50:36 +1100

    Lesson 72 - Middleware Setup - Once I create the middleware, we get a response in the terminal but in the browser we have a problem that the page is always loading. This is because we successfully logged but did not pass it on to the next middleware unless you are specifically terminating by sending back the response. - next() solved the above issue
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/25292564#overview

--- 02-Practice/app.js
+++ 02-Practice/app.js
@@ -1,16 +1,21 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 // req => middleware => res
-app.get('/', (req, res) => {
-    const method = req.method;
-    const url = req.url;
+
+// middleware function
+const logger = (req,res,next) => {
+    const method = req.method
+    const url = req.url
     const time = new Date().getFullYear();
-    console.log(`${method} ${url} ${time}`); // GET / 2021
-  res.send('Home');
+    console.log(`${method} ${url} ${time}`); // Example Terminal Response: GET / 2021
+    next()
+}
+app.get('/', logger,(req, res) => {
+  res.send('Home')
 });
 
-app.get('/about', (req, res) => {
+app.get('/about', logger, (req, res) => {
     res.send('About');
 });
 
