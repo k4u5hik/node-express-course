6ab4b72d9cca6a9634fd03def6d8fac225c37e45
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-12 20:43:58 +1100

    Lesson 73 - APP.USE WEBSTORM SHORTCUTS - CTRL+Cmd+G = select all occurrence - CTRL+G = select next occurrence - can delete or edit once selected - transferred the middleware to logger.js exported from there and then imported inside of app.js - localhost Working! - instead of adding logger manually to each url, app.use is a better solution. - order matters in the middleware it would not invoke on the lines of code above it. if app.get is before app.use then it wont apply to that particular app.get above it but would apply to app.get below the lines where app.use was invoked. - interesting that if you put app.use('/api',logger), it will apply to the url path /api only but not others. If you do have the route then it would apply logger to all app.use(logger)
    http://expressjs.com/en/5x/api.html#app.use
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/25292564#overview

--- 02-Practice/app.js
+++ 02-Practice/app.js
@@ -1,22 +1,26 @@
 const express = require('express')
 const app = express()
-
+const logger = require('./logger.js')
 // req => middleware => res
 
 // middleware function
-const logger = (req,res,next) => {
-    const method = req.method
-    const url = req.url
-    const time = new Date().getFullYear();
-    console.log(`${method} ${url} ${time}`); // Example Terminal Response: GET / 2021
-    next()
-}
-app.get('/', logger,(req, res) => {
+
+app.use('/api',logger)
+
+app.get('/',(req, res) => {
   res.send('Home')
 });
 
-app.get('/about', logger, (req, res) => {
+app.get('/about', (req, res) => {
     res.send('About');
+});
+
+app.get('/api/products',(req, res) => {
+    res.send('Products')
+});
+
+app.get('/api/items', (req, res) => {
+    res.send('Items');
 });
 
 app.listen(3000, () => {
