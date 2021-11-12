e5d1e27592e506b6fc3e78e7e3e10e23d960e057
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-12 21:55:21 +1100

    Lesson 75 - Additional Middleware Info
    app.get('/api/items', [logger,authorize],(req, res) => {
        console.log(req.user)
        res.send('Items');
    });
    
    - Above code to add two or mode middleware functions to the route.
    - third party morgan npm is popular
    - installed morgan
    - app.use morgan tiny
    - GET / 304 - - 0.508 ms
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/25292564#overview

--- 02-Practice/app.js
+++ 02-Practice/app.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 const logger = require('./logger.js')
 const authorize = require('./authorize.js')
+const morgan = require('morgan')
+
 // req => middleware => res
 
 // middleware function
 
-app.use([logger,authorize])
+// use vs route
+// options - your own/ express / third party
+
+app.use([morgan('tiny')])
 
 app.get('/',(req, res) => {
   res.send('Home')
@@ -20,7 +25,7 @@ app.get('/api/products',(req, res) => {
     res.send('Products')
 });
 
-app.get('/api/items', (req, res) => {
+app.get('/api/items', [logger,authorize],(req, res) => {
     console.log(req.user)
     res.send('Items');
 });
