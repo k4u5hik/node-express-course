cbfa5131a495b4952e178ae72d221b86301bd4ec
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-13 22:28:25 +1100

    Lesson 78 - POST Form
    - HTML > Form > action > /login - url path is on our server. When the form elements are filled in we send it to this path.
    - If we enter form data and submit we get an error Cannot POST /login, you check the inspector, it shows 404 error with POST instead of GET.
    - In the headers we see form data and key value pairs of the form input data.
    - When we are sending a GET request the body is optional, however while sending a POST request, the body is crucial.
    - app.post and add middleware
    - urlencoded options
    http://expressjs.com/en/5x/api.html#express.urlencoded
    - if statement to check if name was provided and return information accordingly
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/25292564#overview

--- 02-Practice/app.js
+++ 02-Practice/app.js
@@ -5,13 +5,24 @@ const {people} = require('./data')
 // Static assets
 app.use(express.static('./methods-public'))
 
-// POST METHOD
-
+// Parse form data - urlencoded is built-in express middleware
+app.use(express.urlencoded({extended: false}))
 
 // GET METHOD
 app.get('/api/people', (req, res) => {
   res.status(200).json({success:true, data:people})
 })
+
+// POST METHOD
+app.post('/login', ((req, res) => {
+    console.log(req.body)
+    const {name} = req.body
+    if (name) {
+      return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send(`Please provide credentials`)
+    res.send('POST')
+}))
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000 http://localhost:3000');
