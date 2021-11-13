1e3767d397ad29970066abc74ffb1ea1b390a4f8
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-13 20:30:24 +1100

    Lesson 76 - GET METHOD HTTP METHODS - GET, POST, PUT, DELETE
    - Clearing app.js
    - Using people from data.js
    - Get people data from data.js
    - http://localhost:3000/api/people
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/25292564#overview

--- 02-Practice/app.js
+++ 02-Practice/app.js
@@ -1,34 +1,10 @@
 const express = require('express')
 const app = express()
-const logger = require('./logger.js')
-const authorize = require('./authorize.js')
-const morgan = require('morgan')
+const {people} = require('./data')
 
-// req => middleware => res
-
-// middleware function
-
-// use vs route
-// options - your own/ express / third party
-
-app.use([morgan('combined')])
-
-app.get('/',(req, res) => {
-  res.send('Home')
-});
-
-app.get('/about', (req, res) => {
-    res.send('About');
-});
-
-app.get('/api/products',(req, res) => {
-    res.send('Products')
-});
-
-app.get('/api/items', [logger,authorize],(req, res) => {
-    console.log(req.user)
-    res.send('Items');
-});
+app.get('/api/people', (req, res) => {
+  res.status(200).json({success:true, data:people})
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000 http://localhost:3000');
