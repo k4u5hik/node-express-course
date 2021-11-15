ea0eb7c6190096d2c45f60b21c7716ce025ba62d
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-15 16:07:10 +1100

    Lesson 90 - Setting up basic express server
    - Setup const, app.get and listening port
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27016804#overview

--- 03-Practice/app.js
+++ 03-Practice/app.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//routes
+app.get('/hello', ((req, res) => {
+    res.send('Task Manager App!');
+}));
+
+app.listen(port, () => console.log(`Task Manager app listening on port ${port}! http://localhost:3000`));
