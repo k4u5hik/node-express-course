603eacbaa9cdf381593e14e7a5953e90af3e2715
Kaushik Chemburkar <48584376+k4u5hik@users.noreply.github.com>
2021-11-15 14:24:25 +1100

    Lesson 84 - Express Router - Controllers
    - Created a new folder called controllers. It's a convention to name it as such as per the MVC model.
    - Changed layout by moving around code
    - sub-divided the code into contollers and routes
    - It should work the same
    - Testing GET POST PUT AND DELETE on Postman----- Working!
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/25312092#overview
    
    FREECODECAMP VIDEO END!

--- 02-Practice/routes/people.js
+++ 02-Practice/routes/people.js
@@ -2,65 +2,31 @@ const express = require('express');
 const router = express.Router();
 
 const {people} = require('../data')
+const {  getPeople,
+    createPerson,
+    createPersonPostman,
+    updatePerson,
+    deletePerson} = require('../controllers/people')
 
 // GET METHOD
-router.get('/', (req, res) => {
-    res.status(200).json({success:true, data:people})
-})
+// router.get('/', getPeople)
+//
+// // POST METHOD - Javascript
+// router.post('/', createPerson )
+//
+// // Adding another post method on a different router to test on postman
+// router.post('/postman', createPersonPostman )
+//
+// //PUT METHOD
+// router.put('/:id', updatePerson)
+//
+// //DELETE METHOD
+// router.delete('/:id' , deletePerson)
 
-// POST METHOD - Javascript
-router.post('/', (req, res) => {
-    const {name} = req.body
-    if (!name) {
-        return res
-            .status(400)
-            .json({success:false, msg: 'Name is required'})
-    }
-    res.status(201).send({success:true, person: name})
-})
+// Alternative way to do the above - cleaner and easier to read
 
-
-// Adding another post method on a different router to test on postman
-router.post('/postman', (req, res) => {
-    const {name} = req.body
-    if (!name) {
-        return res
-            .status(400)
-            .json({success:false, msg: 'Name is required'})
-    }
-    res.status(201).send({success:true, data: [...people, name]})
-})
-
-//PUT METHOD
-router.put('/:id', (req, res) => {
-    const {id} = req.params
-    const {name} = req.body
-    const person = people.find(person => person.id === Number(id))
-    if (!person) {
-        return res.status(404).json({success: false, msg: `Person not found with id ${id}`})
-    }
-    const newPeople = people.map(person => {
-        if (person.id === Number(id)) {
-            return {...person, name}
-        }
-        return person
-    })
-    res.status(200).json({success: true, data: newPeople})
-})
-
-//DELETE METHOD
-router.delete('/:id', (req, res) => {
-    const id = req.params.id
-    const person = people.find(person => person.id === Number(id))
-    if (!person) {
-        return res
-            .status(404)
-            .json({success: false, msg: `Person not found with id ${id}`})
-    }
-    const newPeople = people.filter(person => person.id !== Number(id))
-    return res
-        .status(200)
-        .json({success: true, data: newPeople})
-})
+router.route('/').get(getPeople).post(createPerson);
+router.route('/postman').post(createPersonPostman)
+router.route('/:id').put(updatePerson).delete(deletePerson);
 
 module.exports = router;
