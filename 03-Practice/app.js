54f4c332b644b539bb3003ddfb6a00a4c21302dd
Kaushik Chemburkar <attherate@gmail.com>
2021-11-16 11:22:22 +1100

    Lesson 96 - REST API
    - REST stands for Representational State Transfer. Most popular API design patterns, that combines HTTP verbs, route paths and our resources/data. REST determines how the data looks like. It's a pattern, not a strictly enforced set of rules. You can set your own pattern. It is important to be consistent inorder to not confuse users and/or other developers.
    - Just because the URL's are the same it can be a different HTTP request like GET or POST, thus totally different request.
    - JSON is a common format to receive and send data in REST API
    - API allows our user to perform CRUD operations on our data. API interface is built around CRUD.
    - https://en.wikipedia.org/wiki/Representational_state_transfer
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27016834#overview

--- 03-Practice/app.js
+++ 03-Practice/app.js
@@ -20,3 +20,5 @@ app.use('/api/v1/tasks', tasks)
 // app.delete('/api/v1/tasks/:id') delete task
 
 app.listen(port, () => console.log(`Task Manager app listening on port ${port}! http://localhost:${port}`));
+
+//Lesson 96 - Commit notes added
