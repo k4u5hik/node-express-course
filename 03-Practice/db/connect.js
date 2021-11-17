1a8f9da4e2f3db962a2f9ae5f0e56725b461be38
Kaushik Chemburkar <attherate@gmail.com>
2021-11-17 16:18:05 +1100

    Lesson 102 - CRUD on MongoDB Store.Products Lesson 103 - Mongoose - Mongoose is an mongodb object modeling for node.js Lesson 104 - Connect to DB using mongoose. - If you put the wrong password you get Code 8000 Atlas Error - There are a few deprecation warnings but we have connected to the database. - Removed deprecation error from adding the following to mongoose.connect         useNewUrlParser: true,         useCreateIndex: true,         useFindAndModify: false,         useUnifiedTopology: true - No more errors - Connected to the database
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27016866#overview

--- 03-Practice/db/connect.js
+++ 03-Practice/db/connect.js
@@ -1,4 +1,16 @@
+const mongoose = require('mongoose');
+
 // GitGuardian detected this file as sensitive.
 // this file should be in .gitignore
 
-const connectionString = 'mongodb+srv://kaushik:123@nodeexpressprojects.lrdtx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const connectionString = 'mongodb+srv://kaushik:123@nodeexpressprojects.lrdtx.mongodb.net/03-Practice?retryWrites=true&w=majority'
+
+mongoose
+    .connect(connectionString,{
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    })
+    .then(() => {console.log('Connected to database');})
+    .catch((err) => {console.log(err);});
