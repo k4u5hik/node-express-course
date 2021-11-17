e39373097c6c7d2d8a8d5b646e044b56ddcf011c
Kaushik Chemburkar <attherate@gmail.com>
2021-11-17 16:34:30 +1100

    Lesson 105 - Refactor Connect - IMPORTANT
    - If you want to check the connection then try to change the password and you will see the errors relating to server not being connected. MongoError: bad auth : Authentication failed. AtlastError
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27016870#notes

--- 03-Practice/app.js
+++ 03-Practice/app.js
@@ -1,8 +1,8 @@
-require('./db/connect.js')
 const express = require('express');
 const app = express();
 const port = 3000;
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect.js')
 
 //middleware
 app.use(express.json());
@@ -20,6 +20,13 @@ app.use('/api/v1/tasks', tasks)
 // app.patch('/api/v1/tasks/:id') update task
 // app.delete('/api/v1/tasks/:id') delete task
 
-app.listen(port, () => console.log(`Task Manager app listening on port ${port}! http://localhost:${port}`));
+const start = async () => {
+    try {
+        await connectDB();
+        app.listen(port, () => console.log(`Server started on http://localhost:${port}`));
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-//
+start()
--- 03-Practice/db/connect.js
+++ 03-Practice/db/connect.js
@@ -5,12 +5,13 @@ const mongoose = require('mongoose');
 
 const connectionString = 'mongodb+srv://kaushik:123@nodeexpressprojects.lrdtx.mongodb.net/03-Practice?retryWrites=true&w=majority'
 
-mongoose
-    .connect(connectionString,{
+const connectDB = (url) => {
+    return mongoose.connect(connectionString, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
         useUnifiedTopology: true
     })
-    .then(() => {console.log('Connected to database');})
-    .catch((err) => {console.log(err);});
+}
+
+module.exports = connectDB;
