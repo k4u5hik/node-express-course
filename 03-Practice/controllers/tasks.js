70f9c0ae4bc383558a45d457304f3d1c03ad4195
Kaushik Chemburkar <attherate@gmail.com>
2021-11-19 13:26:56 +1100

    Lesson 111 - Try/Catch Block - Catch validation errors
    https://mongoosejs.com/docs/validation.html
    - Mongoose docs for validaion
    - You can have a hard coded error message if you don't want the whole message in the output.
    - Empty values entered - getting error message in postman. Validations working!
    - Name cannot be more than 20 characters error working!
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27016894#notes

--- 03-Practice/controllers/tasks.js
+++ 03-Practice/controllers/tasks.js
@@ -4,8 +4,12 @@ const getAllTasks = (req, res) => {
   res.send('get all tasks')
 }
 const createTask = async (req, res) => {
+  try{
   const task = await Task.create(req.body);
   res.status(201).json({task})
+} catch(error){
+  res.status(500).json({msg: error})
+}
 }
 const getTask = (req, res) => {
   res.json({id: req.params.id})
