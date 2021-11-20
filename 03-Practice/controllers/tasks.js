32ddcd4db48c0c2bb1128a3ea6692eddceab8bf1
Kaushik Chemburkar <attherate@gmail.com>
2021-11-20 20:19:31 +1100

    Lesson 115 - Delete Task
    - Very similar to getTask
    - Instead of findOne we add findOneAndDelete
    - You can have different responses like the ones on line 44,45
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27016910#notes

--- 03-Practice/controllers/tasks.js
+++ 03-Practice/controllers/tasks.js
@@ -34,8 +34,19 @@ const getTask = async (req, res) => {
 const updateTask = (req, res) => {
   res.send('update task')
 }
-const deleteTask = (req, res) => {
-  res.send('delete task')
+const deleteTask = async (req, res) => {
+  try {
+    const {id:taskID} =  req.params
+    const task = await Task.findOneAndDelete({_id: taskID})
+    if(!task){
+      return res.status(404).json({msg: `Task id: ${taskID} not found`}) // ALWAYS RETURN STATUS CODE * Very important*
+    }
+    //res.status(200).json({task}) OR
+    //res.status(200).send() OR
+    res.status(200).json({task:null, status: 'deleted'})
+  } catch (error) {
+    res.status(500).json({msg: error})
+  }
 }
 module.exports = {
   getAllTasks,
