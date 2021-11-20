9f0533cbfeb901e5563ed72525396a453c29ce4b
Kaushik Chemburkar <attherate@gmail.com>
2021-11-20 20:48:42 +1100

    Lesson 116 - Update Task
    - Task.findOneAndUpdate
    - Add key values in the Postman Body
    - new: true returns the updated document
    - runValidators line 37
    - Working!
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27016910#notes

--- 03-Practice/controllers/tasks.js
+++ 03-Practice/controllers/tasks.js
@@ -31,8 +31,17 @@ const getTask = async (req, res) => {
     res.status(500).json({msg: error})
   }
 }
-const updateTask = (req, res) => {
-  res.send('update task')
+const updateTask = async (req, res) => {
+  try {
+    const {id:taskID} = req.params
+    const task = await Task.findOneAndUpdate({_id: taskID}, req.body, {new: true, runValidators:true}) // new: true returns the updated document
+    if(!task){
+      return res.status(404).json({msg: `Task id: ${taskID} not found`})
+    }
+    res.status(200).json({task})
+  } catch (error) {
+    res.status(500).json({msg: error})
+  }
 }
 const deleteTask = async (req, res) => {
   try {
