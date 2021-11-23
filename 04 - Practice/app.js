d1b4844dada9d6250ca92d76b70f0bddba6ccb1b
Kaushik Chemburkar <attherate@gmail.com>
2021-11-23 16:08:08 +1100

    Lesson 128 - START STORE API - Basic Express Setup
    - Copied files from the starter folder
    - npm install
    - npm start
    - Added code on app.js
    - Basic Setup -WORKING!
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27096966#announcements

---
+++ 04 - Practice/app.js
@@ -0,0 +1,34 @@
+require ('dotenv').config()
+//async errors
+
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+
+//middleware
+app.use(express.json())
+
+//routes
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Store API</h1><a href="/api/products">Product Route</a>`)
+})
+
+// products route
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const start = async () =>{
+    try {
+        //connectDB
+        app.listen(port, console.log(`Server is listening http://localhost:${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
