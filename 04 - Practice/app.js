d123fab3d1397d9595ef79bbe58ddf7bc432e9d2
Kaushik Chemburkar <attherate@gmail.com>
2021-11-23 18:51:25 +1100

    Lesson 129 - Connect to DB
    - Went to MongoDB and created a new database 04-STORE-API
    - Connected to the DB from local
    - Server is listening - DB connected!
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27096966#announcements

--- 04 - Practice/app.js
+++ 04 - Practice/app.js
@@ -1,10 +1,12 @@
 require ('dotenv').config()
 //async errors
 
-
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+
+const connectDB = require('./db/connect')
+
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
 
@@ -25,10 +27,10 @@ app.use(errorMiddleware)
 const start = async () =>{
     try {
         //connectDB
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server is listening http://localhost:${port}`))
     } catch (error) {
         console.log(error)
     }
 }
-
 start()
