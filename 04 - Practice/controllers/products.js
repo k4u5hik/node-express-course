fe991b1e8f1e8847edbba4275a78bdfe8473ac01
Kaushik Chemburkar <attherate@gmail.com>
2021-11-26 15:41:19 +1100

    Lesson 135 - Basic Find
    - Postman static returning all the products that were populated from the json file
    - products.js - line 4 - you can pass arguments to get featured products on Postman.
    - number of hits in the res line 5
    - Similarly other properties can be looked for in the same way.
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27136042#questions

--- 04 - Practice/controllers/products.js
+++ 04 - Practice/controllers/products.js
@@ -1,8 +1,8 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({});
-  res.status(200).json({products});
+  const products = await Product.find({name: 'vase table'});
+  res.status(200).json({products, nbHits: products.length});
 };
 
 const getAllProducts = async (req, res) => {
