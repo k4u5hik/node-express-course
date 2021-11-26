6b6b9e470702e2d64a857314fee0487e97e16479
Kaushik Chemburkar <attherate@gmail.com>
2021-11-26 16:47:10 +1100

    Lesson 137 - Mongoose v6 update
    - In Mongoose 6, strictQuery is equal to strict by default. However, you can override this behavior globally.
    
    Lesson 138 - Refactor to QueryObject
    
    - Postman - if we enter a key value in the url params that does not exist in the schema then we will return null object.
    
    {{URL}}/products?featured=false&page=5
    returns
    
    {
        "products": [],
        "nbHits": 0
    }
    
    - queryObject added
    -if we have featured=false params along with an undefined param like page then we return all false featured on the postman .
    { featured: false } in the terminal
    
    - if we only search for page param which is undefined we pass a null object and return all entries in the postman.
    {} in the terminal
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/28792718#questions

--- 04 - Practice/controllers/products.js
+++ 04 - Practice/controllers/products.js
@@ -6,8 +6,14 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  console.log(req.query)
-  res.status(200).json({msg: 'products route'});
+  const {featured} = req.query;
+  const queryObject = {}
+  if (featured) {
+    queryObject.featured = featured=== 'true'? true: false;
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json({products, nbHits: products.length});
 };
 
 module.exports = {
