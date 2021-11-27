d84890181594071edcf65d3b1975a7a2bd5cf043
Kaushik Chemburkar <attherate@gmail.com>
2021-11-27 11:31:06 +1100

    Lesson 139 - Company
    - products.js line 9 - added company to req.query
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27137158#questions/16300706/

--- 04 - Practice/db/connect.js
+++ 04 - Practice/db/connect.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose')
 
 const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
+  return mongoose.connect(url
+      //,
+      //{
+
+    //Mongoose 6 useNewUrlParser, useUnifiedTopology, useFindAndModify, and useCreateIndex are no longer supported options. Mongoose 6 always behaves as if useNewUrlParser, useUnifiedTopology, and useCreateIndex are true, and useFindAndModify is false. Please remove these options from your code.
+//    useNewUrlParser: true,
+//    useCreateIndex: true,
+//    useFindAndModify: false,
+//    useUnifiedTopology: true,
+//  }
+  )
 }
 
 module.exports = connectDB
