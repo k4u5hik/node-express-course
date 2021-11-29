b0b3ab245526a85da33db494dd889fb89dd1cd7b
Kaushik Chemburkar <attherate@gmail.com>
2021-11-29 12:50:30 +1100

    Lesson 147 - Numeric Filters Complete
    - Difficult to understand this concept behind the code at the moment.
    - [field,operator,value] output in the terminal
    - price then rating value showing on the terminal
    price $gt 90, rating $gte 4.5 etc.
    -  Postman working
    
    Ch148 - Not important - Outro
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27152134#questions

--- 04 - Practice/controllers/products.js
+++ 04 - Practice/controllers/products.js
@@ -1,70 +1,73 @@
-const Product = require('../models/product');
+const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product
-      .find({price:{$gt:30}})
+  const products = await Product.find({ price: { $gt: 30 } })
       .sort('price')
       .select('name price')
 
-  res.status(200).json({products, nbHits: products.length});
-};
-
+  res.status(200).json({ products, nbHits: products.length })
+}
 const getAllProducts = async (req, res) => {
-  const {featured, company, name, sort, fields, numericFilters} = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query
   const queryObject = {}
 
   if (featured) {
-    queryObject.featured = featured=== 'true'? true: false
+    queryObject.featured = featured === 'true' ? true : false
   }
   if (company) {
-    queryObject.company = company;
+    queryObject.company = company
   }
   if (name) {
-    queryObject.name = {$regex: name, $options: 'i'}
+    queryObject.name = { $regex: name, $options: 'i' }
   }
-
   if (numericFilters) {
     const operatorMap = {
       '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
       '<': '$lt',
       '<=': '$lte',
-      '>=': '$gte',
-      '=': '$eq'
     }
-    const regEx = /\b(<|<=|>|>=|=)\b/g
-    let filters = numericFilters.replace(regEx, (match) => ` ${operatorMap[match]} `) // replace all matches with operatorMap
-    console.log(filters)
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g
+    let filters = numericFilters.replace(
+        regEx,
+        (match) => `-${operatorMap[match]}-`
+    )
+    const options = ['price', 'rating']
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-')
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) }
+      }
+    })
   }
-
-  console.log(queryObject);
-
+  console.log(queryObject)
   let result = Product.find(queryObject)
-  //sort
+  // sort
   if (sort) {
-    const sortList = sort.split(',').join(' ');
+    const sortList = sort.split(',').join(' ')
     result = result.sort(sortList)
-  } else{
-    result = result.sort('createdAt')
+  } else {
+    result = result.sort('createAt')
   }
 
-  if(fields){
-    const fieldsList = fields.split(',').join(' ');
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ')
     result = result.select(fieldsList)
   }
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
 
-  const page = Number(req.query.page) || 1; // if page is not defined, set to 1
-  const limit = Number(req.query.limit) || 10 // if limit is not defined, it will be 10
-  const skip = (page - 1) * limit; // skip is the number of products to skip
-  result = result.limit(limit).skip(skip);
-
-  // We will have 4 pages - 7,7,7,2 , Total 23 products OR
-  // We have 23 products, we want to display 10 products per page, so we have 3 pages
+  result = result.skip(skip).limit(limit)
+  // 23
+  // 4 7 7 7 2
 
   const products = await result
-  res.status(200).json({products, nbHits: products.length});
+  res.status(200).json({ products, nbHits: products.length })
 }
 
 module.exports = {
+  getAllProducts,
   getAllProductsStatic,
-  getAllProducts
 }
