3e7b0cd2ef8351d429e6757cc326f300b34c7289
Kaushik Chemburkar <attherate@gmail.com>
2021-11-29 11:11:29 +1100

    Lesson 146 - Numeric Filters Setup
    - sort by price in ascending order
    - working on postman
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27152128#questions

--- 04 - Practice/controllers/products.js
+++ 04 - Practice/controllers/products.js
@@ -3,7 +3,7 @@ const Product = require('../models/product');
 const getAllProductsStatic = async (req, res) => {
   const products = await Product
       .find({price:{$gt:30}})
-      .sort('name')
+      .sort('price')
       .select('name price')
 
   res.status(200).json({products, nbHits: products.length});
