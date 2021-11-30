08652151b39c7539b3d94c13e55b9236c382572f
Kaushik Chemburkar <attherate@gmail.com>
2021-11-30 13:43:03 +1100

    Lesson 157 - jsonwebtoken npm package
    Lesson 158 - Sign JWT
    
    - Postman once i pass username and password I get msg and json web token
    - if you copy the token string and paste it in https://jwt.io/ then we can read the payload data, header and signature(part of signature is pvt).
    -
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27290020#notes

--- 05-Practice/controllers/main.js
+++ 05-Practice/controllers/main.js
@@ -4,21 +4,27 @@
 
 // setup authentication so only the request with JWT can access the dashboard route
 
-const login = async (req,res) => {
-    const { username, password } = req.body;
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require('../errors/custom-error');
 
-    const CustomAPIError = require('../errors/custom-error');
+const login = async (req,res) => {
+    const {username, password} = req.body;
+
     //mongoose validation
     //Joi validation
     //check in the controller
 
     // check if username and password exist
-    if(!username || !password) {
-            throw new CustomAPIError('Username or password is missing', 400)
+    if (!username || !password) {
+        throw new CustomAPIError('Username or password is missing', 400)
     }
+    //just for demo, normally provided by the DB
+    const id = new Date().getTime()
+    // try to keep payload small, better experience for the user
+    const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'});
 
     console.log(username, password);
-    res.send('Fake login/Register/Signup Route')
+    res.status(200).json({msg: 'User Created', token});
 }
 
 const dashboard = async (req,res) => {
