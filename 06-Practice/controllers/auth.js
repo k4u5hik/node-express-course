2e07aeb16d79964f12e65aa23578ff520e86512b
Kaushik Chemburkar <attherate@gmail.com>
2021-12-04 21:48:38 +1100

    Lesson 175 - Error Checking - Controller Example
    - auth.js - throw BadRequestError if there are missing fields
    - We are using mongoose validator because we want a custom and more meaningful response
    - postman if fields are missing we are returning an error to provide required fields
    - even if we don't have the if statement, we do get an error however, but it is generated by the system, in this case an internal server error 500 was returned which is still ok but we can decide which is better in case by case basis
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27408838#overview

--- 06-Practice/controllers/auth.js
+++ 06-Practice/controllers/auth.js
@@ -1,7 +1,11 @@
 const User = require('../models/user');
 const {StatusCodes} = require('http-status-codes');
-
+const {BadRequestError} = require('../errors');
 const register = async (req, res) => {
+    const {name, email, password} = req.body;
+    if (!name || !email || !password) {
+        throw new BadRequestError('Please provide all required fields');
+    }
     const user = await User.create({...req.body});
     res.status(StatusCodes.CREATED).json(user);
 }
