776df9c190a1f3344e3789414b03f074df9f7362
Kaushik Chemburkar <attherate@gmail.com>
2021-12-05 19:23:45 +1100

    Lesson 179 - Generate Token - Controller
    - Generate token and send back repsonse with token
    - jwtokens setup like the previous project
    - test out in postman
    - few typos in the code were corrected
    
    {
        "name":"kaushik","email":"kaushik5@mail.com", "password":"secret"
    }
    
    - postman working with new entry and the token was created
    
    {
        "user": {
            "name": "kaushik"
        },
        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MWFjNzY2YzZmYzA0ZTBjM2Y0MmRlYzAiLCJuYW1lIjoia2F1c2hpayIsImlhdCI6MTYzODY5MjQ2MCwiZXhwIjoxNjM4Njk2MDYwfQ.Ij_l2ZoCkpe-6uppvuEOiGhVfGKbc3l_42JiMWa9h5o"
    }
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27425658#overview

--- 06-Practice/controllers/auth.js
+++ 06-Practice/controllers/auth.js
@@ -1,13 +1,14 @@
 const User = require('../models/user');
 const {StatusCodes} = require('http-status-codes');
 const {BadRequestError} = require('../errors');
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 
 
 const register = async (req, res) => {
     const user = await User.create({...req.body});
-    res.status(StatusCodes.CREATED).json({user});
+    const token = jwt.sign({userId:user._id,name: user.name}, 'jwtSecret', {expiresIn: '1h'});
+    res.status(StatusCodes.CREATED).json({user:{name: user.name},token}); // you get the token in the frontend, you can also opt to get back the name and email
 }
 const login = async (req, res) => {
     res.send('login');
