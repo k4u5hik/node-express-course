67ce9a923b97fdadc0862ac39c6afd7dfa24ca85
Kaushik Chemburkar <attherate@gmail.com>
2021-12-05 21:25:46 +1100

    Lesson 180 - Generate Token - Instance Method Part 1
    - Schema Instance Methods documentation
    https://mongoosejs.com/docs/guide.html#methods
    
    - user.getName() - this means we can generate the token using the instance method instead.
    
    - we shall change it back
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27425658#overview

--- 06-Practice/controllers/auth.js
+++ 06-Practice/controllers/auth.js
@@ -8,7 +8,9 @@ const jwt = require('jsonwebtoken');
 const register = async (req, res) => {
     const user = await User.create({...req.body});
     const token = jwt.sign({userId:user._id,name: user.name}, 'jwtSecret', {expiresIn: '1h'});
-    res.status(StatusCodes.CREATED).json({user:{name: user.name},token}); // you get the token in the frontend, you can also opt to get back the name and email
+    res
+        .status(StatusCodes.CREATED)
+        .json({user:{name: user.getName()},token}); // you get the token in the frontend, you can also opt to get back the name and email
 }
 const login = async (req, res) => {
     res.send('login');
