dbcd29086afc3673f135565a4f75deab516f5bf1
Kaushik Chemburkar <attherate@gmail.com>
2021-12-06 18:25:12 +1100

    Lesson 186 & 187
    - Lesson 186 - look for the user in the db and findbyId and then use select and remove password. Not implemented in this project. Just something to keep in mind.
    
    Lesson 187 - Job Model
    - Job.js
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27456790#overview

---
+++ 06-Practice/models/Job.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const JobSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: [true, 'Company is required'],
+      maxlength: [50, 'Company name must be less than 50 characters'],
+  },
+  Position: {
+    type: String,
+    required: [true, 'Position is required'],
+      maxlength: [50, 'Position name must be less than 50 characters'],
+  },
+  Status: {
+    type: String,
+    enum: ['Interview', 'Declined', 'Pending'],
+    default: 'Pending',
+  },
+  createdBy:{
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'User is required'],
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Job', JobSchema);
