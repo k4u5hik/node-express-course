75ffd9adb31cdfb8be9c18dba8543d6da7f5ae44
Kaushik Chemburkar <attherate@gmail.com>
2021-12-08 10:03:28 +1100

    Lesson 189 - Get All Jobs
    - Get all jobs created by a user
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27457128#overview

--- 06-Practice/controllers/jobs.js
+++ 06-Practice/controllers/jobs.js
@@ -3,11 +3,12 @@ const {StatusCodes} = require('http-status-codes');
 const {BadRequestError,NotFoundError} = require('../errors');
 
 const getAllJobs = async (req, res) => {
-    res.send('get all jobs');
+    const jobs = await Job.find({createdBy: req.user.userId}).sort(createdAt);
+    res.status(StatusCodes.OK).json({jobs,count: jobs.length});
 }
 
 const getJob = async (req, res) => {
-    res.send('get job');
+    res.send('get single job');
 }
 
 const createJob = async (req, res) => {
