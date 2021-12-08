91c34236751e0546e391fb715708f5f9e7520532
Kaushik Chemburkar <attherate@gmail.com>
2021-12-09 10:54:37 +1100

    Lesson 194 - Duplicate Error
    - Make mongoose error responses user friendly
    - Mongoose Errors - Validation errors, duplicate errors, cast errors(when id syntax does not match exactly to what mongoose is looking for).
    - We also want to change the status code
    - Try to register with the same email on postman
    - Getting the custom response on postman
    
    {
        "msg": "Email [object Object] already exists"
    }
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27457142#questions

--- 06-Practice/middleware/error-handler.js
+++ 06-Practice/middleware/error-handler.js
@@ -1,11 +1,22 @@
 const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err)
+
+let customError = {
+  //set default status
+  statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+  message: err.message || 'Something went wrong, please try again later.',
+}
+
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  if (err.code && err.code === 11000) {
+    customError.msg = `Email ${err.keyValue} already exists`
+    customError.statusCode = 400
+  }
+  //return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
 module.exports = errorHandlerMiddleware
