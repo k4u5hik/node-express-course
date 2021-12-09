b5e4b0b15936e939bd8ae6133973182b4878b79a
Kaushik Chemburkar <attherate@gmail.com>
2021-12-09 11:38:39 +1100

    Lesson 195 - Custom Error
    - Commented out CustomAPIError
    - found a typo that was causing to not retuning the required response
    
    tried to log in from postman without password
    {
        "email":"madhura@mail.com"
    }
    Response
    {
        "msg": "Please provide email and password"
    }
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27505706#questions

--- 06-Practice/middleware/error-handler.js
+++ 06-Practice/middleware/error-handler.js
@@ -1,16 +1,17 @@
-const { CustomAPIError } = require('../errors')
+//const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
 
 let customError = {
   //set default status
   statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-  message: err.message || 'Something went wrong, please try again later.',
+  msg: err.message || 'Something went wrong, please try again later.',
 }
 
-  if (err instanceof CustomAPIError) {
+/*  if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
-  }
+  }*/
+
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate values, ${Object.keys(err.keyValue)} already exists`
     customError.statusCode = 400
