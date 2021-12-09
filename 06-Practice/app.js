8f2fd5ea385274078456044e15e0c948afe2d3c3
Kaushik Chemburkar <attherate@gmail.com>
2021-12-09 17:33:56 +1100

    Lesson 202 - Deploy to Heroku - Part 2
    - Testing deployment as per video
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27505720#overview

--- 06-Practice/app.js
+++ 06-Practice/app.js
@@ -40,6 +40,10 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
