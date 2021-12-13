a8ca3e769659c59c87090914045145af63fc6e5f
Kaushik Chemburkar <attherate@gmail.com>
2021-12-14 10:08:44 +1100

    Lesson 211 - File/Image Upload Setup - Part 2
    - Added .env MONGO_URI - Go to MongoDB and connect to Application
    - mongodb+srv://kaushik:<password>@nodeexpressprojects.lrdtx.mongodb.net/07-fileupload?retryWrites=true&w=majority
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27936576#questions

--- 07-Practice/app.js
+++ 07-Practice/app.js
@@ -26,7 +26,7 @@ const start = async () => {
     await connectDB(process.env.MONGO_URI);
 
     app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
+      console.log(`Server is listening on port http://localhost:${port}...`)
     );
   } catch (error) {
     console.log(error);
