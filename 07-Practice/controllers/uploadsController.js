d9c18759e8324b70f19b55e5c9c032fe0ddb910f
Kaushik Chemburkar <attherate@gmail.com>
2021-12-15 10:42:12 +1100

    Lesson 217 - Express File Upload Package
    - express-fileupload https://www.npmjs.com/package/express-fileupload
    - app.js , uploadsController.js
    - console.log(req.files)
    - Postman upload image request working!
    - Image is publicly available on the server
    http://localhost:3000//uploads/computer-1.jpeg
    - now we are able to take the src and add it when I am creating the product.
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27937026#questions

--- 07-Practice/controllers/uploadsController.js
+++ 07-Practice/controllers/uploadsController.js
@@ -1,8 +1,17 @@
+const path = require('path');
+
 const {StatusCodes} = require('http-status-codes');
 
 const uploadProductImage = async (req, res) => {
-    console.log(req);
-    res.send('upload Product Image');
+    let productImage = req.files.image;
+    const imagePath = path.join(
+        __dirname,
+        '../public/uploads/' + `${productImage.name}`);
+    await productImage.mv(imagePath);
+    return res.status(StatusCodes.OK).json({
+        image: { src: `/uploads/${productImage.name}` },
+        message: 'Image uploaded successfully',
+    });
 };
 
 module.exports = {
