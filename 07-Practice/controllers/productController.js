7270c1559e0946280b0930d293ed22e6fa5cd08e
Kaushik Chemburkar <attherate@gmail.com>
2021-12-14 21:13:24 +1100

    Lesson 216 - Upload Image - Postman
    - correction: res.status
    - Postman Upload Image - Body - Form Data - Key - Change from Text to File
    - we have to use express-fileupload package to help us pass the response file path from the server into the body
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27936982#questions

--- 07-Practice/controllers/productController.js
+++ 07-Practice/controllers/productController.js
@@ -4,7 +4,7 @@ const {StatusCodes} = require('http-status-codes');
 const createProduct = async (req, res) => {
     console.log(req.body);
     const product = await Product.create(req.body);
-    res.statusCode(StatusCodes.CREATED).json({product});
+    res.status(StatusCodes.CREATED).json({product});
 };
 
 const getAllProducts = async (req, res) => {
