1e09a98752aa1cfbccb4f61a1ed9894055d32179
Kaushik Chemburkar <attherate@gmail.com>
2021-12-15 13:21:33 +1100

    Lesson 219 - Additional Checks
    - check if the image exists, it's an image file with right size
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/27937026#questions

--- 07-Practice/controllers/uploadsController.js
+++ 07-Practice/controllers/uploadsController.js
@@ -1,9 +1,25 @@
 const path = require('path');
-
 const {StatusCodes} = require('http-status-codes');
+const CustomError = require('../errors');
 
 const uploadProductImage = async (req, res) => {
-    let productImage = req.files.image;
+    // if the file exists
+    //check format
+    //check size
+    if (!req.files) {
+        throw new CustomError.BadRequestError('No file was uploaded.');
+    }
+    const productImage = req.files.image;
+
+    if (!productImage.mimetype.startsWith('image')) {
+        throw new CustomError.BadRequestError('The uploaded file needs to be an image.');
+    }
+
+    const maxSize = 1024 * 1024;
+    if (productImage.size > maxSize) {
+        throw new CustomError.BadRequestError('The uploaded file is too big.');
+    }
+
     const imagePath = path.join(
         __dirname,
         '../public/uploads/' + `${productImage.name}`);
