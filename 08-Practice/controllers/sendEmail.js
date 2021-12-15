50dec35c460339c45165fdce29d0009cd6ede9ac
Kaushik Chemburkar <attherate@gmail.com>
2021-12-16 10:36:52 +1100

    Lesson 227 - Ethereal Info and Account
    - getting response on screen from localhost 3000
    - checking ethereal.mail inbox, it was in the mailbox
    - worked!
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/28003222#questions

--- 08-Practice/controllers/sendEmail.js
+++ 08-Practice/controllers/sendEmail.js
@@ -1,5 +1,23 @@
+const nodemailer = require('nodemailer');
 const sendEmail = async (req, res) => {
-    res.send('sendEmail')
+    let testAccount = await nodemailer.createTestAccount();
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        auth: {
+            user: 'krhkuf6mj4wz6ocn@ethereal.email',
+            pass: 'dY1RPu62mjhQ7EAGe7'
+        }
+    });
+
+    let info = await transporter.sendMail({
+        from: '"Fred Foo 👻" <attherate@gmail.com>',
+        to: '"Fred FooFoo 👻" <attherate@gmail.com>',
+        subject: 'Hello ✔',
+        html: '<b>Hello world? Sending email with Node.js</b>'})
+
+    res.json(info)
 }
 
 module.exports = sendEmail
