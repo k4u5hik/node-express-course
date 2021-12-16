a441dd042749b76a1542570d54a31054061ac35e
Kaushik Chemburkar <attherate@gmail.com>
2021-12-16 16:53:50 +1100

    Lesson 229, 230, 231 - SendGrid Info, Sender Identity, Send Emails with SendGrid
    - Lesson 228 working!
    - getting response on screen from localhost 3000
    - checking ethereal.mail inbox, it was in the mailbox
    - worked!
    
    https://sendgrid.com/
    
    - Other choice is Maildom https://github.com/gemmadlou/maildom
    - Integrate sendgrid code into our project
    
    Project Complete
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/28003222#questions

--- 08-Practice/controllers/sendEmail.js
+++ 08-Practice/controllers/sendEmail.js
@@ -1,5 +1,8 @@
 const nodemailer = require('nodemailer');
-const sendEmail = async (req, res) => {
+const sgMail = require('@sendgrid/mail')
+
+//Testing, kept for future reference
+const sendEmailEthereal = async (req, res) => {
     let testAccount = await nodemailer.createTestAccount();
     let transporter = nodemailer.createTransport({
         host: 'smtp.ethereal.email',
@@ -17,6 +20,19 @@ const sendEmail = async (req, res) => {
         subject: 'Hello ✔',
         html: '<b>Hello world? Sending email with Node.js</b>'})
 
+    res.json(info)
+}
+
+const sendEmail = async(req,res)=> {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    const msg = {
+        to: 'k4u5hik@icloud.com', // Change to your recipient
+        from: 'attherate@gmail.com', // Change to your verified sender
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    }
+    const info = await sgMail.send(msg)
     res.json(info)
 }
 
