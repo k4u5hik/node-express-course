ae14be95da84660c39fd330140fcfd4a695b443a
Kaushik Chemburkar <attherate@gmail.com>
2021-12-20 10:21:47 +1100

    Lesson 243 - Basic Routes
    - Done with prompting, added routes too which was not a required task for this exercise. Will keep the route folder and the files in it for now.
    
    Basic Routes and Middleware
    [x] setup / GET Route
    [x] setup express.json() middleware
    [x] setup 404 and errorHandler middleware
    [x] import 'exress-async-errors' package
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/28588829#questions

--- 10-Practice/app.js
+++ 10-Practice/app.js
@@ -1,9 +1,22 @@
 require ('dotenv').config();
+require('express-async-errors')
 
 const express = require('express');
 const app = express();
 
 const connectDB = require('./db/connect');
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
