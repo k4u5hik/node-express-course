6ef1229ec7457cd4319160ac447d2163ecec60d6
Kaushik Chemburkar <attherate@gmail.com>
2021-12-21 11:19:09 +1100

    Lesson 246 - User Model
    - Done easily!
    
    #### User Model
    
    - [x] create models folder and User.js file
    - [x] create schema with name,email, password (all type:String)
    - [x] export mongoose model
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/28589009#questions

---
+++ 10-Practice/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true,'Please add a name'],
+    minlength: [3,'Name must be at least 3 characters'],
+    maxlength: [50,'Name cannot be more than 50 characters']
+  },
+  email: {
+    type: String,
+    required: [true,'Please add an email'],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email'
+    ]
+  },
+  password: {
+    type: String,
+    required: [true,'Please add a password'],
+    minlength: [6,'Password must be at least 6 characters'],
+  },
+  role: {
+    type: String,
+    enum: ['user','admin'],
+    default: 'user'
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
