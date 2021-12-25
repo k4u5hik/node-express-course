429118e0137afaf13c798927a26c1f38b4e20c52
Kaushik Chemburkar <attherate@gmail.com>
2021-12-25 17:41:59 +1100

    Lesson 252 - User Roles
    {
        "name":"Madhura",
        "email":"madhura@mail.com",
        "password":"secret",
        "role":"admin"
    }
    
    - Successful
    - First user is admin, if the countDocument is zero. As we recall that a document refers to an entry/record on mongoDB.
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/28641394#overview

--- 10-Practice/controllers/authController.js
+++ 10-Practice/controllers/authController.js
@@ -3,12 +3,16 @@ const {StatusCodes} = require('http-status-codes');
 const CustomError = require('../errors');
 
 const register = async (req, res) => {
-    const {email} = req.body;
+    const {name,email,password} = req.body;
     const emailExists = await User.findOne({email});
     if (emailExists) {
         throw new CustomError.BadRequestError('Email already exists');
     }
-    const user = await User.create((req.body))
+    // first registered user is an admin
+    const isFirstAccount = await User.countDocuments({})===0;
+    const role = isFirstAccount ? 'admin' : 'user';
+
+    const user = await User.create({name,email,password, role});
     res
         .status(StatusCodes.CREATED)
         .json({user});
