3c3c4286621103b982585d6a59574ad4dd1fca3b
Kaushik Chemburkar <attherate@gmail.com>
2021-12-26 22:40:51 +1100

    Lesson 253 - Hash Passwords
    - [] UserSchema.pre('save') - hook
    - this points to User
    - bcrypt.genSalt - number of rounds
    - bcrypt.hash
    
    - Checked and restarted on MongoDB - Working!
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/28641394#overview

--- 10-Practice/controllers/authController.js
+++ 10-Practice/controllers/authController.js
@@ -10,9 +10,9 @@ const register = async (req, res) => {
     }
     // first registered user is an admin
     const isFirstAccount = await User.countDocuments({})===0;
-    const role = isFirstAccount ? 'admin' : 'user';
+    const role = isFirstAccount ? 'admin' : 'user'; // if true, then admin, else user
 
-    const user = await User.create({name,email,password, role});
+    const user = await User.create({name,email,password,role});
     res
         .status(StatusCodes.CREATED)
         .json({user});
