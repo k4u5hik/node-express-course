2e527e2d2825085e90070fa1aec6e85509ca9df7
Kaushik Chemburkar <attherate@gmail.com>
2021-12-30 15:57:05 +1100

    Lesson 258 - Refactor Cookie Setup
    - Cookie response working though postman
    
    BODY
    {
        "user": {
            "_id": "61cd3b98d2b30762b89a9f7c",
            "name": "barbaloot1",
            "email": "barbaloot1@gmail.com",
            "role": "user"
        }
    }
    
    COOKIES
    token - value
    
    - [x] refactor code, create jwt functions in utils
    - [x] refactor cookie code
    - [x] setup func attachCookiesToResponse
    - [x] accept payload(res, tokenUser)
    - [x] create token, setup cookie
    - [x] optionally send back the response
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/28641418#overview

--- 10-Practice/controllers/authController.js
+++ 10-Practice/controllers/authController.js
@@ -1,41 +1,29 @@
 const User = require('../models/user');
 const {StatusCodes} = require('http-status-codes');
 const CustomError = require('../errors');
-const {createJWT} = require('../utils/jwt');
+const {attachCookiesToResponse} = require('../utils/jwt');
 
 
 const register = async (req, res) => {
-    const {name,email,password} = req.body;
+    const {name, email, password} = req.body;
     const emailExists = await User.findOne({email});
     if (emailExists) {
         throw new CustomError.BadRequestError('Email already exists');
     }
     // first registered user is an admin
-    const isFirstAccount = await User.countDocuments({})===0;
+    const isFirstAccount = await User.countDocuments({}) === 0;
     const role = isFirstAccount ? 'admin' : 'user'; // if true, then admin, else user
 
-    const user = await User.create({name,email,password,role});
+    const user = await User.create({name, email, password, role});
     const tokenUser = {
         _id: user._id,
         name: user.name,
         email: user.email,
         role: user.role
     };
-
-    const token = createJWT({payload: tokenUser});
-
-    const oneDay = 1000*60*60*24; // one day in milliseconds
-
-    res.cookie('token',token, {
-        httpOnly: true,
-        expires: new Date(Date.now() + oneDay),
-    });
-
-    res
-        .status(StatusCodes.CREATED)
-        .json({user: tokenUser});
-};
-
+    attachCookiesToResponse({res, user:tokenUser});
+    //res.status(StatusCodes.CREATED).json({user:tokenUser});
+}
 const login = async (req, res) => {
     res.send('Logged in');
 };
--- 10-Practice/utils/jwt.js
+++ 10-Practice/utils/jwt.js
@@ -9,7 +9,18 @@ const createJWT = ({payload}) => {
 
 const isTokenValid = ({token}) => jwt.verify(token, process.env.JWT_SECRET);
 
+const attachCookiesToResponse = ({res, user}) => {
+    const token = createJWT({payload:user});
+    const oneDay = 24 * 60 * 60 * 1000;
+    res.cookie('token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + oneDay),
+    });
+    res.status(201).json({user});
+};
+
 module.exports = {
     createJWT,
-    isTokenValid
+    isTokenValid,
+    attachCookiesToResponse
 };
