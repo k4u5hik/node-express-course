30ef461a0cbceb30cf466d170c7b1c72bf526797
Kaushik Chemburkar <attherate@gmail.com>
2022-01-08 15:59:40 +1100

    Lesson 270 - ShowMe Controller
    - it doesnt query the database, it uses authentication.js from the data stored in the cookies. If the token is valid then it will get name,user and role.
    - Go to Postman and check if showme route works.
    
    GET /api/v1/users/showMe 401 31 - 3.836 ms
    POST /api/v1/auth/login 201 106 - 130.787 ms
    GET /api/v1/users/showMe 200 76 - 4.383 ms
    
    Worked!
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/28714676#overview

--- 10-Practice/routes/userRoutes.js
+++ 10-Practice/routes/userRoutes.js
@@ -12,7 +12,7 @@ const {
 
 router.route('/').get(authenticateUser,authorisePermissions('admin','owner'), getAllUsers); // We need to authenticate the user in the first order then we check if the user is admin
 
-router.route('/showMe').get(showCurrentUser);//if the location from above :id to below it, it shows showMe as the current user. We don't want that.
+router.route('/showMe').get(authenticateUser,showCurrentUser);//if the location from above :id to below it, it shows showMe as the current user. We don't want that.
 router.route('/updateUser').patch(updateUser);
 router.route('/updateUserPassword').patch(updateUserPassword);
 
