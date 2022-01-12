0b11a8d4f7d2f3abccf80538b15dcd4fb77cd373
Kaushik Chemburkar <attherate@gmail.com>
2022-01-12 16:29:08 +1100

    Lesson 278 - Product Routes - Structure
    Did the task on my own with a couple prompts.
    
    #### Product Structure
    
    - [x] add productController file in controllers
    - [x] export (createProduct, getAllProducts,
      getSingleProduct, updateProduct, deleteProduct, uploadImage) functions
    - [x] res.send('function name')
    - [x] setup productRoutes file in routes
    - [x] import all controllers
    - [x] only getAllProducts and getSingleProduct accessible to public
    - [x] rest only by admin (setup middlewares)
    - [x] typical setup
    - [x] router.route('/uploadImage').post(uploadImage)
    - [x] import productRoutes as productRouter in the 1-intro.js
    - [x] setup app.use('/api/v1/products', productRouter)
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/28750596#overview

--- 10-Practice/app.js
+++ 10-Practice/app.js
@@ -9,8 +9,9 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./db/connect');
 
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
+const authRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -28,8 +29,9 @@ app.get('/api/v1', (req, res) => {
   res.send('eCommerce API');
 });
 
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
---
+++ 10-Practice/routes/productRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const {authenticateUser, authorisePermissions} = require('../middleware/authentication');
+
+const {
+    createProduct,
+    getAllProducts,
+    getSingleProduct,
+    updateProduct,
+    deleteProduct,
+    uploadImage,
+} = require('../controllers/productController');
+
+router.route('/')
+    .get(getAllProducts)
+    .post(authenticateUser, authorisePermissions, createProduct);
+
+router.route('/:id/uploadImage')
+    .put(authenticateUser, authorisePermissions, uploadImage);
+router.route('/:id')
+    .get(getSingleProduct)
+    .patch(authenticateUser, authorisePermissions, updateProduct)
+    .delete(authenticateUser, authorisePermissions, deleteProduct);
+
+
+module.exports = router;
