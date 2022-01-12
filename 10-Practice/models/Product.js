d338e9b41899aa60e6f60c9263092d9f34261a85
Kaushik Chemburkar <attherate@gmail.com>
2022-01-12 15:35:42 +1100

    Lesson 277 - Product Model
    #### Product Model
    
    - [x] create Product.js in models folder
    - [x] create Schema
    - [x] name : {type:String}
    - [x] price: {type:Number}
    - [x] description: {type:String}
    - [x] image: {type:String}
    - [x] category: {type:String}
    - [x] company: {type:String}
    - [x] colors: {type:[]}
    - [x] featured: {type:Boolean}
    - [x] freeShipping: {type:Boolean}
    - [x] inventory:{type:Number}
    - [x] averageRating:{type:Number}
    - [x] user
    - [x] set timestamps
    - [x] export Product model
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/28718138#overview

---
+++ 10-Practice/models/Product.js
@@ -0,0 +1,82 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+        unique: true,
+        trim: true, // removes white space
+        maxlength: [50, 'Name can not be more than 50 characters'],
+    },
+    price: {
+        type: Number,
+        required: [true, 'Please add a price'],
+        trim: true,
+        default: 0,
+        min: [0, 'Price must be greater than 0'],
+    },
+    description: {
+        type: String,
+        required: [true, 'Please add a description'],
+        trim: true,
+        maxlength: [500, 'Description can not be more than 500 characters'],
+    },
+    image: {
+        type: String,
+        required: [true, 'Please add an image'],
+        trim: true,
+    },
+    category: {
+        type: String,
+        required: [true, 'Please add a category'],
+        trim: true,
+        enum: ['Office', 'Kitchen', 'Bedroom', 'Other'],
+    },
+    company: {
+        type: String,
+        required: [true, 'Please add a company'],
+        trim: true,
+        enum: {
+            values: ['Samsung', 'Apple', 'LG', 'Other'],
+            message: '{VALUE} is not supported'
+        },
+    },
+    colours: {
+        type: [String],
+        required: [true, 'Please add at least one colour'],
+        default: ['#000000'],
+        enum: {
+            values: ['Black', 'White', 'Red', 'Blue', 'Green', 'Yellow', 'Purple', 'Orange', 'Pink', 'Other'],
+            message: '{VALUE} is not supported'
+        },
+    },
+    featured: {
+        type: Boolean,
+        default: false,
+    },
+    freeShipping: {
+        type: Boolean,
+        default: false,
+    },
+    inventory: {
+        type: Number,
+        required: [true, 'Please add an inventory'],
+        default: 0,
+        min: [0, 'Inventory must be greater than 0'],
+    },
+    averageRating: {
+        type: Number,
+        default: 0,
+    },
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+},
+    {timestamps: true}
+);
+
+module.exports = mongoose.model('Product', ProductSchema);
