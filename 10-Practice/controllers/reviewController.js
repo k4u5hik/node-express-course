f1c5f7c43614ce719dca0a3709c6481881457952
Kaushik Chemburkar <attherate@gmail.com>
2022-01-15 22:41:37 +1100

    Lesson 289 - updateReview Controller
    - [x] get id from req.params
    - [x] get {rating, title comment} from req.body
    - [x] check if review exists
    - [x] if no review, 404
    - [x] check permissions
    - [x] set review properties equal to rating, title, comment
    - [x] use await review.save()
    - [x] send back 200
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/28806582#questions

--- 10-Practice/controllers/reviewController.js
+++ 10-Practice/controllers/reviewController.js
@@ -43,7 +43,18 @@ const getSingleReview = async (req, res) => {
 }
 
 const updateReview = async (req, res) => {
-    res.send('updateReview');
+    const {id: reviewId} = req.params;
+    const {rating, title, comment} = req.body;
+    const review = await Review.findOne({_id: reviewId});
+    if (!review) {
+        throw new CustomError.NotFoundError(`Review with id ${reviewId} does not exist`);
+    }
+    checkPermissions(req.user, review.user);
+    review.rating = rating;
+    review.title = title;
+    review.comment = comment;
+    await review.save();
+    res.status(StatusCodes.OK).json({review});
 }
 
 const deleteReview = async (req, res) => {
