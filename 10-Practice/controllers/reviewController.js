669e78279379f36bc6b124936d47110efa070ac6
Kaushik Chemburkar <attherate@gmail.com>
2022-01-16 17:03:02 +1100

    Lesson 291 - Mongoose Virtuals
    - You want to attach reviews to the when getSingleProduct or getAllProducts controllers
    - Mongoose virtuals are properties that do not persist or are stored in the database, but only exist logically. We apply when we compute something.
    - In case of review, in the model we can see the reference of user and product, however no such reference can be found in the product model in this case.
    - You can find the reviews with rating 1 - it does not show if the review is updated to any other number.
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/28806588#questions

--- 10-Practice/controllers/reviewController.js
+++ 10-Practice/controllers/reviewController.js
@@ -43,7 +43,15 @@ const getAllReviews = async (req, res) => {
 
 const getSingleReview = async (req, res) => {
     const {id: reviewId} = req.params;
-    const review = await Review.findOne({_id: reviewId});
+    const review = await Review.findOne({_id: reviewId})
+        .populate({
+        path: 'product',
+        select: 'name company price'
+    })
+        .populate({
+            path: 'user',
+            select: 'name'
+        });;
     if (!review) {
         throw new CustomError.NotFoundError(`Review with id ${reviewId} does not exist`);
     }
