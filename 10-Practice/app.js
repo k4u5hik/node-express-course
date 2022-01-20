a6f4804b0172db02908c0c92294ad108f49a523b
Kaushik Chemburkar <attherate@gmail.com>
2022-01-21 10:29:05 +1100

    Lesson 307 - Security Packages
    - [x] express-rate-limiter
    - [x] helmet
    - [x] xss-clean
    - [x] express-mongo-sanitize
    - [x] cors (cookies!!!!)
    
    Ready to deploy Heroku application
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/28838486#questions

--- 10-Practice/app.js
+++ 10-Practice/app.js
@@ -8,6 +8,12 @@ const app = express();
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
+//security packages
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean')
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const connectDB = require('./db/connect');
 
@@ -19,6 +25,16 @@ const orderRouter = require('./routes/orderRoutes');
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.set('trust proxy', 1);
+app.use('rate-limit', rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 60 // limit each IP to 100 requests per windowMs
+}));
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use(morgan('tiny'));
 app.use(express.json());
