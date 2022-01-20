1d38e5907ecc391c4b7de96c4652b9b52664f41b
Kaushik Chemburkar <attherate@gmail.com>
2022-01-20 14:16:47 +1100

    Lesson 302 - createOrder Complete
    A bit challenging.
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/28886714#questions

--- 10-Practice/controllers/orderController.js
+++ 10-Practice/controllers/orderController.js
@@ -1,5 +1,41 @@
+const Order = require('../models/order');
+const Product = require('../models/Product');
+const {StatusCodes} = require('http-status-codes');
+const CustomError = require('../errors');
+const {checkPermissions} = require('../utils');
+
 const createOrder = async (req,res) => {
+    const {items: cartItems, tax, shippingFee} = req.body;
+
+    if (!cartItems || cartItems.length < 1) {
+        throw new CustomError.BadRequestError('Cart is empty');
+    }
+    if (!tax || !shippingFee) {
+        throw new CustomError.BadRequestError('Tax or shipping fee is missing');
+    }
+
+    let orderItems = [];
+    let subtotal = 0;
+
+    for (const item of cartItems) {
+        const dbProduct = await Product.findOne({_id:item.product});
+        if (!dbProduct) {
+            throw new CustomError.NotFoundError(`Product not found with id ${item.product}`);
+        }
+        const {name, price, image, _id} = dbProduct;
+        //console.log(name, price, image, _id);
+        const singleOrderItem = {
+            amount: item.amount,
+            name, price, image, product: _id,
+        };
+        //add item to order
+        orderItems = [...orderItems, singleOrderItem];
+        //add item to subtotal, we don't overwrite subtotal, += is used
+        subtotal += item.amount * price;
+    }
     res.send('create order');
+    console.log(orderItems);
+    console.log(subtotal);
 }
 
 const getAllOrders = async (req,res) => {
