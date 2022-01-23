8340ee75e42e562f75987bcdc2e4cc2d36815907
Kaushik Chemburkar <attherate@gmail.com>
2022-01-24 10:59:22 +1100

    Lesson 316,317,318 - Verify Email - Setup and Complete
    - [x] create verifyEmail in authController
    - [x] get verificationToken and email from req.body
    - [x] setup a '/verify-email' route in authRoutes
    - [x] test in a Postman
    - [x] check for user using email
    - [x] if no user 401
    - [x] if token does not match user token 401
    - [x] if correct set
    - [x] user.isVerified = true
    - [x] user.verified = Date.now()
    - [x] user.verificationToken = ''
    - [x] save use with instance method
    - [x] return msg:'email verified'
    
    {{URL}}/auth/verify-email
    
    {
        "email": "w4jbfestevmedsoa@ethereal.email",
        "verificationToken": "bf8de86c454d2fa6739a3d6dfcef6e61249bc9c073181d2f6b5f6555bfcf7e79384edd2f3421b046"
    }
    
    {
        "msg": "Email verified successfully"
    }
    
    - Able to login that user now
    
    {
        "email": "w4jbfestevmedsoa@ethereal.email",
        "password":"secret"
    }
    
    {
        "user": {
            "name": "Carli Stiedemann",
            "userId": "61ede5dd38e35eba40506e8d",
            "role": "user"
        }
    }
    
    - Deleting all the users after this exercise.
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/28990602#questions

--- 11-Practice/server/controllers/authController.js
+++ 11-Practice/server/controllers/authController.js
@@ -26,6 +26,26 @@ const register = async (req, res) => {
     verificationToken: user.verificationToken,
   });
 }
+
+const verifyEmail = async (req,res)=>{
+  const {verificationToken,email} = req.body;
+  const user = await User.findOne({email});
+  if(!user){
+    throw new CustomError.UnauthenticatedError('Invalid verification token');
+  }
+  if(user.verificationToken !== verificationToken){
+    throw new CustomError.UnauthenticatedError('Invalid verification token');
+  }
+  user.isVerified = true;
+  user.verified = Date.now();
+  user.verificationToken = "";
+  await user.save();
+
+  res.status(StatusCodes.OK).json({
+    msg: 'Email verified successfully'
+  });
+}
+
 const login = async (req, res) => {
   const { email, password } = req.body;
 
@@ -61,4 +81,5 @@ module.exports = {
   register,
   login,
   logout,
+  verifyEmail
 };
