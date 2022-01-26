2fc80e02bcba7ffb2d308eb36a1cd44c849bd322
Kaushik Chemburkar <attherate@gmail.com>
2022-01-27 09:06:20 +1100

    Lesson 326 - Attaching Multiple Cookies to a response
    - We can control the expiration with a cookie so we do not really need JWT expiration
    - Usually,the access token is shorter duration, refresh token is longer.
    - After logging in from the Postman I can see two cookies
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/29015050#overview

--- 11-Practice/server/utils/jwt.js
+++ 11-Practice/server/utils/jwt.js
@@ -1,15 +1,34 @@
 const jwt = require('jsonwebtoken');
 
 const createJWT = ({ payload }) => {
-  const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_LIFETIME,
-  });
+  const token = jwt.sign(payload, process.env.JWT_SECRET);
   return token;
 };
 
 const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
 
-const attachCookiesToResponse = ({ res, user }) => {
+const attachCookiesToResponse = ({ res, user, refreshToken }) => {
+  const accessTokenJWT = createJWT({ payload: {user} });
+  const refreshTokenJWT = createJWT({ payload: {user,refreshToken} });
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  res.cookie('accessToken', accessTokenJWT, {
+    httpOnly: true,
+    maxAge: 1000,// in milliseconds
+    secure: process.env.NODE_ENV === 'production',
+    signed: true,
+  });
+
+  res.cookie('refreshToken', refreshTokenJWT, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === 'production',
+    signed: true,
+  });
+};
+
+// Copied for reference, not exported
+/*const attachSingleCookieToResponse = ({ res, user }) => {
   const token = createJWT({ payload: user });
 
   const oneDay = 1000 * 60 * 60 * 24;
@@ -20,7 +39,7 @@ const attachCookiesToResponse = ({ res, user }) => {
     secure: process.env.NODE_ENV === 'production',
     signed: true,
   });
-};
+};*/
 
 module.exports = {
   createJWT,
