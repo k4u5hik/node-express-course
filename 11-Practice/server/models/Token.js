ed533fbd906b76efc59b29c6bf11c546a86ccd20
Kaushik Chemburkar <attherate@gmail.com>
2022-01-26 16:24:56 +1100

    Lesson 325 - Create Token in Login Controller
    - we assume that the user does not have the token
    
    Login from Postman
    
    {
        "user": {
            "name": "kaushik",
            "userId": "61ef3bc67d63828a87410349",
            "role": "admin"
        },
        "token": {
            "refreshToken": "41ef05d04362a98043c8295e03d9db082d9705222a700c95914905e0026f628cea36db7c463cf572",
            "ip": "127.0.0.1",
            "userAgent": "PostmanRuntime/7.29.0",
            "isValid": true,
            "user": "61ef3bc67d63828a87410349",
            "_id": "61f0dad7a63ff5ea6e4cad14",
            "__v": 0
        }
    }
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/29015050#overview

--- 11-Practice/server/models/Token.js
+++ 11-Practice/server/models/Token.js
@@ -16,7 +16,7 @@ const TokenSchema = new mongoose.Schema({
     },
     isValid: {
         type: Boolean,
-        required: true
+        default: true
     },
     user: {
         type: mongoose.Types.ObjectId,
