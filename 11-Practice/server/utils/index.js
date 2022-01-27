0b9792b9d127252f1174ff3ae8ebdddf32c49fbb
Kaushik Chemburkar <attherate@gmail.com>
2022-01-27 20:09:14 +1100

    Lesson 333 - Send Reset Password Email
    - Reset Password working from the browser
    - Noticed an unathentication error in the terminal
    
    UnauthenticatedError: Authentication Invalid
        at authenticateUser (/Users/kaushik/WebstormProjects/node-express-course-v2/11-Practice/server/middleware/authentication.js:31:11)
        at newFn (/Users/kaushik/WebstormProjects/node-express-course-v2/11-Practice/server/node_modules/express-async-errors/index.js:16:20)
        at Layer.handle [as handle_request] (/Users/kaushik/WebstormProjects/node-express-course-v2/11-Practice/server/node_modules/express/lib/router/layer.js:95:5)
        at next (/Users/kaushik/WebstormProjects/node-express-course-v2/11-Practice/server/node_modules/express/lib/router/route.js:137:13)
        at Route.dispatch (/Users/kaushik/WebstormProjects/node-express-course-v2/11-Practice/server/node_modules/express/lib/router/route.js:112:3)
        at newFn (/Users/kaushik/WebstormProjects/node-express-course-v2/11-Practice/server/node_modules/express-async-errors/index.js:16:20)
        at Layer.handle [as handle_request] (/Users/kaushik/WebstormProjects/node-express-course-v2/11-Practice/server/node_modules/express/lib/router/layer.js:95:5)
        at /Users/kaushik/WebstormProjects/node-express-course-v2/11-Practice/server/node_modules/express/lib/router/index.js:281:22
        at Function.process_params (/Users/kaushik/WebstormProjects/node-express-course-v2/11-Practice/server/node_modules/express/lib/router/index.js:335:12)
        at next (/Users/kaushik/WebstormProjects/node-express-course-v2/11-Practice/server/node_modules/express/lib/router/index.js:275:10) {
      statusCode: 401
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/29062968#questions

--- 11-Practice/server/utils/index.js
+++ 11-Practice/server/utils/index.js
@@ -2,11 +2,13 @@ const { createJWT, isTokenValid, attachCookiesToResponse } = require('./jwt');
 const createTokenUser = require('./createTokenUser');
 const checkPermissions = require('./checkPermissions');
 const sendVerificationEmail = require('./sendVerificationEmail');
+const sendResetPasswordEmail = require('./sendResetPasswordEmail');
 module.exports = {
   createJWT,
   isTokenValid,
   attachCookiesToResponse,
   createTokenUser,
   checkPermissions,
-  sendVerificationEmail
+  sendVerificationEmail,
+  sendResetPasswordEmail
 };
