d42cb48e0c4ac7b8431e621e6c3a06f68c9de48c
Kaushik Chemburkar <attherate@gmail.com>
2022-01-27 16:47:00 +1100

    Lesson 332 - Forgot Password Controller
    - MongoDB - We have passwordTokenExpirationDate and passwordToken both
    
    {"_id":{"$oid":"61f22efaef74458f803d7ab9"},
    "name":"kaushik","email":"kaushik@gmail.com",
    "password":"$2a$10$A.vT.hEUgbnzolGbDS/jze2uw.6DVzIZehtwL0vSbroPBQfRw2.va","role":"admin",
    "verificationToken":"",
    "isVerified":true,
    "__v":{"$numberInt":"0"},
    "verified":{"$date":{"$numberLong":"1643261729519"}},"passwordToken":"ad65a6ff767b12f2464ce9b9ee2c64673c40c4411c0db15b8a8d188397b862dd81e4c0655564f2491c995144907bf6cc592646ed3161024b6c6350dfef78a4616809759e49b5",
    "passwordTokenExpirationDate":{"$date":{"$numberLong":"1643262797786"}}}
    
    https://www.udemy.com/course/nodejs-tutorial-and-projects-course/learn/lecture/29062968#questions

--- 11-Practice/server/controllers/authController.js
+++ 11-Practice/server/controllers/authController.js
@@ -129,7 +129,26 @@ const logout = async (req, res) => {
 };
 
 const forgotPassword = async (req,res)=> {
-  res.send('forgot password')
+  const {email}  = req.body
+  if (!email) {
+    throw new CustomError.BadRequestError('Please provide email');
+  }
+
+  const user = await User.findOne({email});
+  if (user) {
+    const passwordToken = crypto.randomBytes(70).toString('hex');
+    // send email
+
+    const tenMinutes = 1000 *60 * 10
+    const passwordTokenExpirationDate = new Date(Date.now() + tenMinutes);
+    user.passwordToken = passwordToken;
+    user.passwordTokenExpirationDate = passwordTokenExpirationDate;
+    await user.save();
+  }
+
+  res.status(StatusCodes.OK).json({
+    msg: 'Please check your email to reset your password'
+  });
 }
 
 const resetPassword = async (req,res)=> {
