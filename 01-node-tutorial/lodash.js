00770e143d29b5ff2c329f7caa5cbc7cd561b463
Kaushik Chemburkar <attherate@gmail.com>
2022-07-05 23:24:03 +1000

    lodash - trial 2

--- 01-node-tutorial/lodash.js
+++ 01-node-tutorial/lodash.js
@@ -16,3 +16,37 @@ const compact = _.compact([0, 1, false, 2, '', 3]);
 // => [1, 2, 3]
 console.log(compact);
 
+const difference = _.difference([3, 2, 1], [2, 3]);
+// => [1]
+console.log(difference);
+
+const differenceBy = _.differenceBy([2.1, 1.2], [2.3, 3.4], Math.floor);
+// => [1.2]
+console.log(differenceBy);
+
+const drop1 = _.drop([1, 2, 3]);
+// => [2, 3]
+console.log(drop1);
+ 
+const drop2 = _.drop([1, 2, 3], 2);
+// => [3]
+console.log(drop2);
+ 
+const drop3 = _.drop([1, 2, 3], 5);
+// => []
+console.log(drop3);
+ 
+const drop4 = _.drop([1, 2, 3], 0);
+// => [1, 2, 3]
+console.log(drop4);
+
+_.defer(function(stamp) {
+    console.log(_.now() - stamp);
+  }, _.now());
+  // => Logs the number of milliseconds it took for the deferred invocation.
+
+const timestamp = _.now();
+console.log(timestamp);
+
+const isDate = _.isDate(new Date());
+console.log(isDate);
