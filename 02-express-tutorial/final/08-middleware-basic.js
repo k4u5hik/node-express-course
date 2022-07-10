86bc8632ef441547f79206da3edb171da8d961d4
Kaushik Chemburkar <attherate@gmail.com>
2022-07-10 11:08:22 +1000

    JS getMonth shows previous month 0-11 - fixed

--- 02-express-tutorial/final/08-middleware-basic.js
+++ 02-express-tutorial/final/08-middleware-basic.js
@@ -6,7 +6,7 @@ const app = express()
 const logger = (req, res, next) => {
   const method = req.method
   const url = req.url
-  const time = new Date().getFullYear() + '-' + new Date().getMonth() + '-' + new Date().getDate() + ' ' + new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds()
+  const time = new Date().getFullYear() + '-' + (new Date().getMonth()+1) + '-' + new Date().getDate() + ' ' + new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds()
   console.log(method, url, time)
   next()
 }
