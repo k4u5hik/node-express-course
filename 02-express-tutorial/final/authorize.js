fc037f89c34dbc97fbaa3d396301a5e61f163b29
Kaushik Chemburkar <attherate@gmail.com>
2022-07-12 13:38:00 +1000

    console log - which user logged in

--- 02-express-tutorial/final/authorize.js
+++ 02-express-tutorial/final/authorize.js
@@ -2,6 +2,7 @@ const authorize = (req, res, next) => {
     const {user} = req.query
     if (user === 'admin') {
         req.user = {name: 'Kaushik', id: 3}
+        console.log(req.user.name)
         next()
     } else {
         res.status(401).send('Unauthorized')
